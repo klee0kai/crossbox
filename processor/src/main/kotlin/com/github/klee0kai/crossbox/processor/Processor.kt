8361368b2143b088f80955f8984c577894e009f2
klee0kai <klee0kai@gmail.com>
2025-03-01 23:04:48 +0400

    in one iteration we take symbols according to the number of processors

--- processor/src/main/kotlin/com/github/klee0kai/crossbox/processor/Processor.kt
+++ processor/src/main/kotlin/com/github/klee0kai/crossbox/processor/Processor.kt
@@ -1,19 +1,20 @@
-@file:OptIn(KspExperimental::class)
-
 package com.github.klee0kai.crossbox.processor
 
 import com.github.klee0kai.crossbox.processor.ksp.GenSpec
 import com.github.klee0kai.crossbox.processor.ksp.TargetFileProcessor
-import com.google.devtools.ksp.KspExperimental
 import com.google.devtools.ksp.processing.CodeGenerator
 import com.google.devtools.ksp.processing.KSPLogger
 import com.google.devtools.ksp.processing.Resolver
 import com.google.devtools.ksp.processing.SymbolProcessor
 import com.google.devtools.ksp.symbol.KSAnnotated
 import com.squareup.kotlinpoet.ksp.writeTo
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import java.util.concurrent.ConcurrentLinkedQueue
+import java.util.concurrent.atomic.AtomicInteger
+import kotlin.math.max
+import kotlin.math.min
 
 
 class Processor(
@@ -23,7 +24,9 @@ class Processor(
 ) : SymbolProcessor {
 
     companion object {
-        val PROJECT_URL = "https://github.com/klee0kai/crossbox"
+        const val PROJECT_URL = "https://github.com/klee0kai/crossbox"
+
+        val ONE_RUN_SYMBOLS_COUNT = max(Runtime.getRuntime().availableProcessors(), 4)
     }
 
     val targetProcessors = arrayOf<TargetFileProcessor>(
@@ -33,24 +36,40 @@ class Processor(
 
     override fun process(
         resolver: Resolver
-    ): List<KSAnnotated> = runBlocking {
+    ): List<KSAnnotated> = runBlocking(Dispatchers.Default) {
 
-        val symbolsForReprocessing = ConcurrentLinkedQueue<KSAnnotated>()
+        val globalSymbolsForProcessing = ConcurrentLinkedQueue<KSAnnotated>()
+        val globalSymbolsForReprocessing = ConcurrentLinkedQueue<KSAnnotated>()
         val genSpecs = ConcurrentLinkedQueue<GenSpec>()
+        val processSymbolsCounter = AtomicInteger(0)
 
         val generateCodeJob = launch {
             targetProcessors.map { processor ->
                 val symbols = processor.findSymbolsToProcess(resolver)
-                symbolsForReprocessing.addAll(symbols.symbolsForReprocessing)
+                var takeSymbolsCount = 0
+                processSymbolsCounter.updateAndGet { totalCount ->
+                    takeSymbolsCount = min(symbols.symbolsForProcessing.size, ONE_RUN_SYMBOLS_COUNT - totalCount)
+                    takeSymbolsCount = max(takeSymbolsCount, 0)
+                    totalCount + takeSymbolsCount
+                }
+
+                val symbolsForReprocessing = (symbols.symbolsForReprocessing
+                        + symbols.symbolsForProcessing.drop(takeSymbolsCount))
+                val symbolsForProcessing = symbols.symbolsForProcessing.take(takeSymbolsCount)
+
+                globalSymbolsForReprocessing.addAll(symbolsForReprocessing)
+                globalSymbolsForProcessing.addAll(symbolsForProcessing)
+
                 genSpecs.addAll(
-                    symbols.symbolsForProcessing.mapNotNull { targetSymbol ->
-                        processor.process(
-                            targetSymbol = targetSymbol,
-                            resolver = resolver,
-                            options = options,
-                            logger = logger,
-                        )
-                    }
+                    symbolsForProcessing
+                        .mapNotNull { targetSymbol ->
+                            processor.process(
+                                targetSymbol = targetSymbol,
+                                resolver = resolver,
+                                options = options,
+                                logger = logger,
+                            )
+                        }
                 )
             }
         }
@@ -61,15 +80,13 @@ class Processor(
         generateCodeJob.join()
 
         genSpecs.forEach { genSpec ->
-            launch {
-                genSpec?.fileSpec?.writeTo(
-                    codeGenerator = codeGenerator,
-                    dependencies = genSpec.dependencies
-                )
-            }
+            genSpec?.fileSpec?.writeTo(
+                codeGenerator = codeGenerator,
+                dependencies = genSpec.dependencies
+            )
         }
 
-        symbolsForReprocessing.toList()
+        globalSymbolsForReprocessing.toList()
     }
 
     override fun finish() {
