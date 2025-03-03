7d5b9efdf4f106dab9f1e1a54b66a45c8efe4d89
klee0kai <klee0kai@gmail.com>
2025-03-03 14:31:02 +0400

    update test

--- tests/src/test/kotlin/com/github/klee0kai/crossbox/tests/SimpleModelsTests.kt
+++ tests/src/test/kotlin/com/github/klee0kai/crossbox/tests/SimpleModelsTests.kt
@@ -2,15 +2,15 @@ package com.github.klee0kai.crossbox.tests
 
 import com.github.klee0kai.crossbox.example.SimpleModel
 import com.github.klee0kai.crossbox.example.crossbox.fieldList
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class SimpleModelsTests {
 
     @Test
-    fun test1() {
+    fun testFieldList() {
         val list = SimpleModel.fieldList
-//        println("hello world ${list}")
-        assert(true)
+        assertEquals(listOf("name", "number", "long", "short"), list.map { it.name })
     }
 
 }
